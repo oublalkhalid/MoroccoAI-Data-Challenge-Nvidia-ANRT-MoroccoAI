b38600092bc9cb5164d4325f4c0138e25f78e703
khalid OUBLAL <76509145+oublalkhalid@users.noreply.github.com>
2021-12-22 20:34:53 +0100

    Add files via upload

---
+++ code/settings.py
@@ -0,0 +1,16 @@
+DEBUG= True 
+
+
+# Yolo character detection palte complet
+images_input= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train'
+annotations_input_xml= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_annotation_xml'
+labels_output = '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_annotation_txt'
+config_output= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/'
+
+
+
+# Yolo character detection(a,b,h,d,...)
+images_input_character= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_charcter_detection/image'
+annotations_input_xml_character= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_charcter_detection/label_xml'
+labels_output_character = '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_charcter_detection/label_txt'
+config_output_character= '/home/koublal/Downloads/moroccoai-data-challenge-edition-001/train_charcter_detection/' 
